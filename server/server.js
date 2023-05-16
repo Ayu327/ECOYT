d3b36800ba4c517f79e1477d8d07adde8df1c69f
Ayu327 <ayusaini327@gmail.com>
2023-05-16 21:44:20 +0530

    connection established

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 import  express  from 'express';
 import colors  from 'colors'
+import morgan from 'morgan';
 //npm i colors
 
 
 import dotenv from 'dotenv'
+import connectDb from './config/db.js';
 
 //configure env
 dotenv.config();
+
+
+//database config
+connectDb()
+
 //rest object
 const app = express()
+
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
 
 //rest API
 app.get("/",(req,res)=>{
