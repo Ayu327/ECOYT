24efa7e54d3acdefb0723f2c7f61c8f9d9f4ad7a
Ayu327 <ayusaini327@gmail.com>
2023-05-23 18:53:15 +0530

    fetched_from_backend

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -13,35 +13,31 @@ const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  // form function
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const res = await axios.post("/api/v1/auth/login", {
-//         email,
-//         password,
-//       });
-//       if (res && res.data.success) {
-//         toast.success(res.data && res.data.message);
-//         setAuth({
-//           ...auth,
-//           user: res.data.user,
-//           token: res.data.token,
-//         });
-//         localStorage.setItem("auth", JSON.stringify(res.data));
-//         navigate(location.state || "/");
-//       } else {
-//         toast.error(res.data.message);
-//       }
-//     } catch (error) {
-//       console.log(error);
-//       toast.error("Something went wrong");
-//     }
-//   };
+ // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        const res = await axios.post("/api/v1/auth/login", {
+        
+          email,
+          password
+        
+        });
+        if (res && res.data.success) {
+          toast.success(res.data && res.data.message);
+          navigate("/");
+        } else {
+          toast.error(res.data.message);
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error("Something went wrong");
+      }
+    };
   return (
     <Layout title="Register - Ecommer App">
       <div className="form-container " style={{ minHeight: "90vh" }}>
-        <form >
+        <form onSubmit={handleSubmit}>
           <h4 className="title">LOGIN FORM</h4>
 
           <div className="mb-3">
