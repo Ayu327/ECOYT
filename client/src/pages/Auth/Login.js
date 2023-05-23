70eb6840b841b82f22d5f538d9ce5afe2ff97a18
Ayu327 <86731124+Ayu327@users.noreply.github.com>
2023-05-23 18:45:10 +0530

    Update Login.js

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useNavigate, useLocation } from "react-router-dom";
 import toast from "react-hot-toast";
 import "./RegisterStyle.css";
-// import { useAuth } from "../../Context/AuthContext";
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -13,31 +13,7 @@ const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  // form function
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const res = await axios.post("/api/v1/auth/login", {
-//         email,
-//         password,
-//       });
-//       if (res && res.data.success) {
-//         toast.success(res.data && res.data.message);
-//         setAuth({
-//           ...auth,
-//           user: res.data.user,
-//           token: res.data.token,
-//         });
-//         localStorage.setItem("auth", JSON.stringify(res.data));
-//         navigate(location.state || "/");
-//       } else {
-//         toast.error(res.data.message);
-//       }
-//     } catch (error) {
-//       console.log(error);
-//       toast.error("Something went wrong");
-//     }
-//   };
+  
   return (
     <Layout title="Register - Ecommer App">
       <div className="form-container " style={{ minHeight: "90vh" }}>
