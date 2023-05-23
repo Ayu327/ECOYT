2aef73902e436306ce539e49326449829f69a6ef
Ayu327 <ayusaini327@gmail.com>
2023-05-23 19:12:29 +0530

    COntext_working_checked_through_Home.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Layout from '../component/Layout/Layout'
+import { useAuth } from '../Context/AuthContext'
 
 const HomePage = () => {
+  const[auth,setAuth] = useAuth()
   return (
     <div>
      
             <h1>HomePage</h1>
+            <pre>{JSON.stringify(auth,null,4)}</pre>
     
       
     </div>
