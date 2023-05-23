32ace902d30f68554d555d69be68e59b24da1f4f
Ayu327 <ayusaini327@gmail.com>
2023-05-23 19:07:21 +0530

    Context_Created_Index_updated

--- client/src/Context/AuthContext.js
+++ client/src/Context/AuthContext.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext, createContext } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
@@ -9,20 +9,20 @@ const AuthProvider = ({ children }) => {
   });
 
   //default axios
-  axios.defaults.headers.common["Authorization"] = auth?.token;
+//   axios.defaults.headers.common["Authorization"] = auth?.token;
 
-  useEffect(() => {
-    const data = localStorage.getItem("auth");
-    if (data) {
-      const parseData = JSON.parse(data);
-      setAuth({
-        ...auth,
-        user: parseData.user,
-        token: parseData.token,
-      });
-    }
-    //eslint-disable-next-line
-  }, []);
+//   useEffect(() => {
+//     const data = localStorage.getItem("auth");
+//     if (data) {
+//       const parseData = JSON.parse(data);
+//       setAuth({
+//         ...auth,
+//         user: parseData.user,
+//         token: parseData.token,
+//       });
+//     }
+//     //eslint-disable-next-line
+//   }, []);
   return (
     <AuthContext.Provider value={[auth, setAuth]}>
       {children}
