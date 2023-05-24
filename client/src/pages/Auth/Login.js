471daddf914bcbebf818159642e3378ebb14bad4
Ayu327 <ayusaini327@gmail.com>
2023-05-24 12:53:56 +0530

    login_checking

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -4,36 +4,40 @@ import axios from "axios";
 import { useNavigate, useLocation } from "react-router-dom";
 import toast from "react-hot-toast";
 import "./RegisterStyle.css";
-// import { useAuth } from "../../Context/AuthContext";
+ import { useAuth } from "../../Context/AuthContext";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-//   const [auth, setAuth] = useAuth();
+  const [auth, setAuth] = useAuth();
 
   const navigate = useNavigate();
-  const location = useLocation();
+//   const location = useLocation();
 
- // form function
+  // form function
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        const res = await axios.post("/api/v1/auth/login", {
-        
-          email,
-          password
-        
+      const res = await axios.post("/api/v1/auth/login", {
+        email,
+        password,
+      });
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
         });
-        if (res && res.data.success) {
-          toast.success(res.data && res.data.message);
-          navigate("/");
-        } else {
-          toast.error(res.data.message);
-        }
-      } catch (error) {
-        console.log(error);
-        toast.error("Something went wrong");
+        // localStorage.setItem("auth", JSON.stringify(res.data));
+         navigate( "/");
+      } else {
+        toast.error(res.data.message);
       }
-    };
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
   return (
     <Layout title="Register - Ecommer App">
       <div className="form-container " style={{ minHeight: "90vh" }}>
