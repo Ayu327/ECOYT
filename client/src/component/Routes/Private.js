552f7858cfb3caa77a416f04d5348996af4e9e57
Ayu327 <ayusaini327@gmail.com>
2023-05-24 19:12:32 +0530

    Spinner_useEffect_added

--- client/src/component/Routes/Private.js
+++ client/src/component/Routes/Private.js
@@ -4,15 +4,14 @@ import { useAuth } from "../../Context/AuthContext";
  import axios from "axios";
 import Spinner from "../Spinner";
 
+
 export default function PrivateRoute() {
   const [ok, setOk] = useState(false);
   const [auth, setAuth] = useAuth();
 
   useEffect(() => {
     const authCheck = async () => {
-      const res = await axios.get("/api/v1/auth/user-auth",
-     
-      );
+      const res = await axios.get("/api/v1/auth/user-auth");
       if (res.data.ok) {
         setOk(true);
       } else {
--- client/src/component/Spinner.js
+++ client/src/component/Spinner.js
@@ -1,16 +1,31 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 const Spinner = () => {
+  const [count, setCount] = useState(5);
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevValue) => --prevValue);
+    }, 1000);
+    count === 0 &&
+      navigate("/login");
+    return () => clearInterval(interval);
+  }, [count, navigate]);
   return (
-   <>
-  <div className="d-flex justify-content-center align-items-center" style={{height:"100vh"}}>
-  <div className="spinner-border" role="status">
-    <span className="visually-hidden"></span>
-  </div>
-</div>
+    <>
+      <div
+        className="d-flex flex-column justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+         <h1 className="Text-center">redirecting to you in {count} second </h1> 
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden"></span>
+        </div>
+      </div>
+    </>
+  );
+};
 
-   </>
-  )
-}
-
-export default Spinner
+export default Spinner;
