7ff11aaeecd9c6c055b2742d70c44814fbc5dca3
Ayu327 <ayusaini327@gmail.com>
2023-06-02 00:54:11 +0530

    Create_CRUD_created

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,8 +1,49 @@
 import React from 'react'
 import Layout from '../../component/Layout/Layout'
 import AdminMenu from '../../component/Layout/AdminMenu'
-
+import { useEffect, useState } from 'react'
+import toast from "react-hot-toast";
+import axios from 'axios';
+import CategoryForm from '../../component/Form/CategoryForm';
 const CreateCategory = () => {
+  const [categories,setcategories] = useState([]);
+  const[name,setName] = useState("")
+
+  //handle Form
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+    try{
+      const {data} = await axios.post('/api/v1/category/create-category', {name})
+      if(data?.success){
+        toast.success(`${name} is created`)
+        getAllCategory()
+      }else{
+        toast.error(data.message)
+      }
+
+    }catch(error){
+      console.log(error)
+      toast.error('somethhing went wrong in input form')
+    }
+  }
+
+
+  //get all cat
+  const getAllCategory = async()=>{
+    try{
+      const {data} = await axios.get('/api/v1/category/get-category')
+      if(data.success){
+        setcategories(data.category)
+      }
+    }catch(error){
+      console.log(error)
+      toast.error('Something went wrong in getting category')
+    }
+  };
+
+  useEffect(()=>{
+    getAllCategory()
+  },[])
   return (
     <Layout title={"Dashboard - Create Category"}>
     <div className='container-fluid m-3 p-3'>
@@ -11,7 +52,37 @@ const CreateCategory = () => {
           <AdminMenu/>
         </div>
         <div className='cold-md-9'>
-          <h1>Create Category</h1>
+          <h1>Manage Category</h1>
+          <div className='p-3 w-50'><CategoryForm handleSubmit={handleSubmit} value={name} setValue={setName}/></div>
+
+          <div className='w-75'>
+          <table className="table">
+  <thead>
+    <tr>
+      
+      <th scope="col">Name</th>
+      <th scope="col">Actions</th>
+   
+    </tr>
+  </thead>
+  <tbody>
+  {categories?.map(c =>(
+    <>
+    <tr>
+    
+        <td key={c._id}>{c.name}</td>
+        <td>
+          <button className='btn btn-primary'>Edit</button>
+        </td>
+      
+    </tr>
+    </>
+    ))}
+  
+  </tbody>
+</table>
+
+          </div>
         </div>
       </div>
       
